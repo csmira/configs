24cf4092a37e1db5e693012c0b2f4fcde8faa4d0
csmira <cristian_smira@hotmail.ca>
2024-02-06 00:13:08 -0500

    feat(nvim): remap ctrl-[ to ESC

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
+vim.keymap.set("i", "<C-[", "Esc")
 vim.keymap.set("v", "J", ":move '>+1<CR>gv-gv")
 vim.keymap.set("v", "K", ":move '<-2<CR>gv-gv")
 
